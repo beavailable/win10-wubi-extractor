394a2a165f5e82f63e38314ee98ff2e7326b8373
beavailable <beavailable@protonmail.com>
2021-03-16 13:18:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# win10-wubi-extractor
+本项目用于提取 win10 系统自带的五笔词库。
+
+以下是两个词库路径：
+```
+C:\Windows\InputMethod\CHS\ChsWubi.lex
+C:\Windows\InputMethod\CHS\ChsWubiNew.lex
+```
+
+# 用法
+```
+./main.py src-file dest-file
+```
+
+# 关于
+本项目是根据 [gitee.com/cnaya/MSWUBI](https://gitee.com/cnaya/MSWUBI) 项目反向编码而来的。
---
+++ main.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print(f'usage: {os.path.basename(sys.argv[0])} src dest')
+    sys.exit(1)
+
+src = sys.argv[1]
+dest = sys.argv[2]
+
+with open(src, 'rb') as sf:
+    data = sf.read()
+
+size = len(data)
+idx = 168
+with open(dest, 'w') as df:
+    while idx < size:
+        total_len = data[idx] | data[idx + 1] << 8
+        idx += 4
+        code_len = (data[idx] | data[idx + 1] << 8) * 2
+        idx += 2
+        code = data[idx:idx + code_len].decode('utf-16')
+        idx += 8
+        value_len = total_len - 16
+        value = data[idx:idx + value_len].decode('utf-16')
+        idx += value_len + 2
+        df.write(f'{code} {value}\n')
